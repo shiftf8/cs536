8899bf7cddb38535057a7b95c91077ec60e598a5
Rob <shiftf8@gmail.com>
2014-09-23 02:49:09 +0000

    assuming given functions work. balanced and later functions need work

--- h2.stackbalancefunction.cpp
+++ h2.stackbalancefunction.cpp
@@ -3,6 +3,7 @@
 //Hart
 //09/22/14
 
+#include <cstdio>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -21,45 +22,62 @@ void push( Stack & stack, const string & item );
 unsigned elements( const Stack & stack );
 
 bool balanced( const string & line );
+bool isLeftParenChecker( const char & c);
 
 bool die( const string & msg );
 
-int main(){
+int main() {
+    string something = "((((((((((((((((((((((";
     
+    balanced(something);
     //system("pause");
     return 0;
 } //main()
 
-void initialize( Stack & stack ){
+void initialize( Stack & stack ) {
     stack.elements = 0;
 }
-string pop( Stack & stack ){
+string pop( Stack & stack ) {
     if (stack.elements == 0) die("stack underflow");
     return stack.data[stack.elements--];
 }
-string top( const Stack & stack ){
+string top( const Stack & stack ) {
     if (stack.elements == 0) die("stack underflow");
     return stack.data[stack.elements];
 }
-void push( Stack & stack, const string & item ){
+void push( Stack & stack, const string & item ) {
     if (stack.elements == STACK_SIZE) die("stack overflow");
     stack.data[++stack.elements] = item;
 }
-unsigned elements( const Stack & stack ){
+unsigned elements( const Stack & stack ) {
     return stack.elements;
 }
 
-bool balanced( const string & line ){
+bool balanced( const string & line ) {
     Stack stackToCheck;
-    unsigned i = 0;
-    
-    initialize(stackToCheck);
-    while (getc(line[i]) != '\n'){
-        push(stackToCheck, line[i++]);
+    bool leftFlag = false;
+    int i = 0;
+    char c;
+    string leftSide;
+
+    while (c != EOF) {
+        c = line[i];
+        if (isLeftParenChecker(c)) {
+            leftSide = c;
+            push(stackToCheck, leftSide);
+        }
+        i++;
     }
 }
 
-bool die( const string & msg ){
+bool isLeftParenChecker( const char & c) {
+    char leftParens[3] = {'(', '[', '{'};
+    
+    for (unsigned i = 0; i < 3; i++) if (leftParens[i] == c) return c;
+    return false;
+}
+
+bool die( const string & msg ) {
 	//cerr <<endl <<"Fatal error: " <<msg << endl;
 	//exit( EXIT_FAILURE );
 
