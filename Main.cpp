aa274badf6f74bd6c1b4697258b49dc5d354271d
Rob Lamog <shiftf8@gmail.com>
2017-08-29 00:49:30 +0000

    First class 2017.

---
+++ Main.cpp
@@ -0,0 +1,7 @@
+#include “PlainBox.h”
+// Create a PlainBox object
+PlainBox box;
+// Set the item in the box
+box.setItem(22.5);
+// Get and print the item in the box
+cout << box.getItem() << endl;
