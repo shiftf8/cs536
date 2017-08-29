f3bd1fc21bb469dcdbbde79a7ebe8a1c50abdaca
Rob Lamog <shiftf8@gmail.com>
2017-08-29 01:28:33 +0000

    First example runs

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,14 @@
-#include “PlainBox.h”
-// Create a PlainBox object
-PlainBox box;
-// Set the item in the box
-box.setItem(22.5);
-// Get and print the item in the box
-cout << box.getItem() << endl;
+#include "PlainBox.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Create a PlainBox object
+    PlainBox box;
+    // Set the item in the box
+    box.setItem(22.5);
+    // Get and print the item in the box
+    cout << box.getItem() << endl;
+    
+    return 0;
+}
