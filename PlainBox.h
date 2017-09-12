aff3d3eb4cb48e241a47f3696f4fa67a79033169
Rob Lamog <shiftf8@gmail.com>
2017-09-12 02:44:46 +0000

    Start Project 1

--- PlainBox.h
+++ PlainBox.h
@@ -6,16 +6,16 @@ typedef double ItemType;
 class PlainBox
 {
 private:
-// Data field
-ItemType item;
+    // Data field
+    ItemType item;
 public:
-// Default constructor
-PlainBox();
-// Parameterized constructor
-PlainBox(const ItemType& theItem);
-// Method to change the value of the data field
-void setItem(const ItemType& theItem);
-// Method to get the value of the data field
-ItemType getItem() const;
+    // Default constructor
+    PlainBox();
+    // Parameterized constructor
+    PlainBox(const ItemType& theItem);
+    // Method to change the value of the data field
+    void setItem(const ItemType& theItem);
+    // Method to get the value of the data field
+    ItemType getItem() const;
 }; // end PlainBox
 #endif
---
+++ Polynomial.h
@@ -0,0 +1,8 @@
+#ifndef POLYNOMIAL_
+#define POLYNOMIAL_
+
+void addTerm(int coefficient, int exponent)
+int degree()
+int coefficient(int power)
+int numberOfTerms()
+int value(int x)
