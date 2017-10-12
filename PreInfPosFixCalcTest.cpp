42de4acc4b7cc9845498c7a285cf62cdef3e9022
Rob Lamog <shiftf8@gmail.com>
2017-10-12 16:21:31 +0000

    Stuck on seg fault issues in precedence sequence.

--- PreInfPosFixCalcTest.cpp
+++ PreInfPosFixCalcTest.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 static const size_t MAX_EXPRESSION_SIZE = 128;
 
-string infixToPostfix(string exp);
+string infixToPostfix(string expression);
 
 int main()
 {
@@ -19,11 +19,6 @@ int main()
     input_expressions = fopen("input.PreInfPosFixCalc.txt", "r");
     if (input_expressions == NULL) perror("Error opening file.\n");
     
-    // fscanf(input_expressions, "%c", &c);
-    // cout << c << endl;
-    // expression.push_back(c);
-    // cout << expression << endl;
-    
     while (fscanf(input_expressions, "%c", &c) != EOF)
     {
         if (c >= 'A' && c <= 'Z') expression.push_back(c);
@@ -32,22 +27,37 @@ int main()
         {
             while (operator_stack.top() != '(')
             {
-                expression.push_back(operator_stack.pop());
+                expression.push_back(operator_stack.top());
+                operator_stack.pop();
             }
             operator_stack.pop();
         }
         if (c == '*' || c == '/' || c == '+' || c == '-')
         {
-            
+            // while ((operator_stack.top() != '(') || (!operator_stack.empty()))
+            // {
+            //     // //Split conditional for easier reading
+            //     // if ((c == '*' || c == '/') && (operator_stack.top() == '+' || operator_stack.top() == '-')) //Check if current operator is of higher precedence
+            //     // {
+            //     //     expression.push_back(operator_stack.top());
+            //     //     operator_stack.pop();
+            //     // }
+            //     // else if (c == operator_stack.top()) //Check if current operator is of equal precedence
+            //     // {
+            //     //     expression.push_back(operator_stack.top());
+            //     //     operator_stack.pop();
+            //     // }
+            // }
+            operator_stack.push(c);
         }
     }
-    
+
     fclose(input_expressions);
 
     return 0;
 }
 
-string infixToPostfix(string exp)
+string infixToPostfix(string expression)
 {
     
 }
