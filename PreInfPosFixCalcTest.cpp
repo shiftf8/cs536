3cefaec60e6a5df40828e761cdab627936889a10
Rob Lamog <shiftf8@gmail.com>
2017-10-12 02:19:07 +0000

    Went with fscanf. Need to figure out operator precedence in expression.

--- PreInfPosFixCalcTest.cpp
+++ PreInfPosFixCalcTest.cpp
@@ -1,4 +1,4 @@
-#include <cstdio>
+#include <iostream>
 #include <stack>
 #include <string>
 
@@ -10,17 +10,36 @@ string infixToPostfix(string exp);
 
 int main()
 {
-    stack<string> operator_stack;
+    stack<char> operator_stack;
     string expression;
+    char c;
     
     FILE* input_expressions;
     
     input_expressions = fopen("input.PreInfPosFixCalc.txt", "r");
     if (input_expressions == NULL) perror("Error opening file.\n");
     
-    while (fgets(expression, MAX_EXPRESSION_SIZE, input_expressions) != NULL)
+    // fscanf(input_expressions, "%c", &c);
+    // cout << c << endl;
+    // expression.push_back(c);
+    // cout << expression << endl;
+    
+    while (fscanf(input_expressions, "%c", &c) != EOF)
     {
-        
+        if (c >= 'A' && c <= 'Z') expression.push_back(c);
+        if (c == '(') operator_stack.push(c);
+        if (c == ')')
+        {
+            while (operator_stack.top() != '(')
+            {
+                expression.push_back(operator_stack.pop());
+            }
+            operator_stack.pop();
+        }
+        if (c == '*' || c == '/' || c == '+' || c == '-')
+        {
+            
+        }
     }
     
     fclose(input_expressions);
