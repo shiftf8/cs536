300ab34239327399510cebf6824d26760cef83bf
Rob Lamog <shiftf8@gmail.com>
2017-10-27 04:00:30 +0000

    Reached a decent stopping point.

--- PreInfPosFixCalcTest.cpp
+++ PreInfPosFixCalcTest.cpp
@@ -2,75 +2,29 @@
 #include <stack>
 #include <string>
 
-using namespace std;
-
 static const size_t MAX_EXPRESSION_SIZE = 128;
 
-string infixToPostfix(string expression);
-bool isLowerPrecedence(char current_operator, char top_of_operator_stack);
+std::string infixToPostfix(std::string expression);
 
 int main()
 {
-    stack<char> operator_stack;
-    string output_expression;
-    char c;
+    std::stack<char> operator_stack;
+    std::string input_expression;
+    std::string output_expression;
 
-
-    FILE* file_input;
-    
-    file_input = fopen("input.PreInfPosFixCalc.txt", "r");
-    if (file_input == NULL) perror("Error opening file.\n");
-    
-    while ((c = fgetc(file_input)) != EOF)
+    while (getline(std::cin, input_expression))
     {
-        if (c >= 'A' && c <= 'Z') output_expression.push_back(c);
-        if (c == '(') operator_stack.push(c);
-        if (c == ')')
-        {
-            while (operator_stack.top() != '(')
-            {
-                output_expression.push_back(operator_stack.top());
-                operator_stack.pop();
-            }
-            operator_stack.pop();
-        }
-        if (c == '*' || c == '/' || c == '+' || c == '-')
-        {
-            while (!operator_stack.empty())
-            {
-                if (operator_stack.top() == '(')
-                {
-                    operator_stack.pop();
-                    break;
-                }
-                if (!isLowerPrecedence(c, operator_stack.top()))
-                {
-                    output_expression.push_back(operator_stack.top());
-                    operator_stack.pop();
-                }
-            }
-            operator_stack.push(c);
-        }
-        if (c == '\n')
-        {
-            cout << output_expression << endl;
-            output_expression.clear();
-            while(!operator_stack.empty()) operator_stack.pop();
-        }
+        std::cout <<infixToPostfix(input_expression) << std::endl;
     }
-
-    fclose(file_input);
 
     return 0;
 }
 
-string infixToPostfix(string expression)
+std::string infixToPostfix(std::string expression)
 {
-    
-}
-bool isLowerPrecedence(char current_operator, char top_of_operator_stack)
-{
-    if (current_operator == top_of_operator_stack) return false;
-    if ((current_operator == '*' || current_operator == '/') && (top_of_operator_stack == '+' || top_of_operator_stack == '-')) return false;
-    return true;
+    while ()
+    {
+        break;
+    }
+    return "x";
 }
