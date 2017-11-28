f62d5ee1b6ba60bdd1e022c350277554cb18e1a8
Rob Lamog <shiftf8@gmail.com>
2017-11-28 02:30:24 +0000

    I think it's finished. Seems short and easy enough. No error checking or handling.

--- ArithExpTreeMain.cpp
+++ ArithExpTreeMain.cpp
@@ -1,13 +1,25 @@
+#include <iostream>
 #include <stack>
 #include <string>
-#include "BTNode.h"
+#include "BTNode.cpp"
 
 using namespace std;
 
 BTNode* makeArithmeticExpressionTree(string postfix);
+void postOrderPrint(BTNode* root);
 
 int main()
 {
+    string post_fix_expression;
+    BTNode* root;
+    
+    while (getline(cin, post_fix_expression))
+    {
+        cout <<post_fix_expression <<endl;
+        root = makeArithmeticExpressionTree(post_fix_expression);
+        postOrderPrint(root);
+        cout <<endl;
+    }
 
     return 0;
 }
@@ -15,6 +27,40 @@ int main()
 BTNode* makeArithmeticExpressionTree(string postfix)
 {
     stack<BTNode*> bt_node_ptrs;
+    char c;
     
-    return bt_node_ptrs.top();
+    for (int i = 0; i < postfix.length(); ++i)
+    {
+        BTNode* tmp;
+        c = postfix[i];
+        
+        if (c >= 'A' && c <= 'Z')
+        {
+            tmp = new BTNode(c);
+            bt_node_ptrs.push(tmp);
+        }
+        if (c == '*' || c == '/' || c == '+' || c == '-')
+        {
+            BTNode* p1;
+            BTNode* p2;
+            
+            p1 = bt_node_ptrs.top();
+            bt_node_ptrs.pop();
+            p2 = bt_node_ptrs.top();
+            bt_node_ptrs.pop();
+            
+            tmp = new BTNode(c, p2, p1);
+            bt_node_ptrs.push(tmp);
+        }
+    }
+    
+    return bt_node_ptrs.empty() ? nullptr : bt_node_ptrs.top();
+}
+void postOrderPrint(BTNode* root)
+{
+    if (root == nullptr) return;
+    
+    postOrderPrint(root->getLeft());
+    postOrderPrint(root->getRight());
+    cout <<root->getItem();
 }
