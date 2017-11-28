d489b7e133898a4a4d8f8a7a36adea0d0138ba64
Rob Lamog <shiftf8@gmail.com>
2017-11-28 01:24:13 +0000

    Started

--- ArithExpTreeMain.cpp
+++ ArithExpTreeMain.cpp
@@ -1,7 +1,20 @@
+#include <stack>
+#include <string>
 #include "BTNode.h"
+
+using namespace std;
+
+BTNode* makeArithmeticExpressionTree(string postfix);
 
 int main()
 {
 
     return 0;
+}
+
+BTNode* makeArithmeticExpressionTree(string postfix)
+{
+    stack<BTNode*> bt_node_ptrs;
+    
+    return bt_node_ptrs.top();
 }
