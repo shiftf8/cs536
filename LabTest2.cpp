cbeea3c61b91ddb5884920e0d7121acab476d7c4
Rob Lamog <shiftf8@gmail.com>
2017-12-05 01:53:43 +0000

    LabTest2.cpp

---
+++ LabTest2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+struct Node
+{
+int data;
+Node* left;
+Node* right;
+};
+
+bool equal(Node* r1, Node* r2);
+int find_max(Node* r);
+
+int main()
+{
+    
+    return 0;
+} //main
+
+bool equal(Node* r1, Node* r2)
+{
+    
+}
+int find_max(Node* r)
+{
+    
+}
